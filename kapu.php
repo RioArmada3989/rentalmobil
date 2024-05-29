b9aa2277716a620164ddf354a372d2b163fc6d18
RioArmada3989 <119870443+RioArmada3989@users.noreply.github.com>
2024-05-29 18:46:24 +0700

    Update kapu.php

--- kapu.php
+++ kapu.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Spreadsheet; error
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
 
